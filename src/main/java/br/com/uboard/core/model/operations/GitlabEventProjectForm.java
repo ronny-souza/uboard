5b7c4f90dbbf866b7a117961cff5a8779728b738
Ronyeri Marinho <ronyeri@usto.re>
2024-10-30 16:47:04 -0300

    ♻️ refactor: ajustando nomes de classes

--- src/main/java/br/com/uboard/core/model/operations/GitlabEventProjectForm.java
+++ src/main/java/br/com/uboard/core/model/operations/GitlabEventProjectForm.java
@@ -4,14 +4,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 /**
- * Representation of the project payload received from the webhook for Gitlab merge request events.
+ * Representation of the project payload received from the webhook for Gitlab events.
  *
  * @author Ronyeri Marinho
  * @since 1.0
  */
 
-@Schema(description = "Attributes of the project targeted by the merge request")
-public record MergeRequestProjectForm(
+@Schema(description = "Attributes of the project targeted by the Gitlab webhook event")
+public record GitlabEventProjectForm(
         @NotNull
         @Schema(description = "Project identifier", example = "1234")
         Long id
