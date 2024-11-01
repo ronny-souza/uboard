7105cb11161680e2bab1791073bfde1e5afb8a62
Ronyeri Marinho <ronyeri@usto.re>
2024-11-01 06:34:10 -0300

    ✨ feat: melhorando a documentação dos endpoints da aplicação

--- src/main/java/br/com/uboard/core/controller/WebhookController.java
+++ src/main/java/br/com/uboard/core/controller/WebhookController.java
@@ -4,8 +4,15 @@ import br.com.uboard.core.model.operations.ReceiveMergeRequestEventForm;
 import br.com.uboard.core.model.operations.ReceiveTagEventForm;
 import br.com.uboard.core.service.CreateIssueService;
 import br.com.uboard.core.service.CreateTagReleaseNotesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -36,15 +43,49 @@ public class WebhookController {
         this.createTagReleaseNotesService = createTagReleaseNotesService;
     }
 
+    @Operation(summary = "Listener for merge request operations, responsible for storing the issues that will make up the release notes.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "The merge request has been received and the issue has been persisted if it does not already exist."
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "One or more fields in the request body are invalid.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    }
+            ),
+    })
     @PostMapping("/merge-request")
-    public ResponseEntity<Void> receiveMergeRequestsEvent(@RequestBody @Valid ReceiveMergeRequestEventForm payload) {
+    public ResponseEntity<Void> receiveMergeRequestEvent(@RequestBody @Valid ReceiveMergeRequestEventForm payload) {
         this.createIssueService.create(payload);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Listener for tag creation operations, responsible for retrieving stored issues and creating release notes for the tag.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "The tag has been received and release notes have been created."
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "One or more fields in the request body are invalid.",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProblemDetail.class)
+                            )
+                    }
+            ),
+    })
     @PostMapping("/tag")
     public ResponseEntity<Void> receiveTagEvent(@RequestBody @Valid ReceiveTagEventForm payload) {
         this.createTagReleaseNotesService.create(payload);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
